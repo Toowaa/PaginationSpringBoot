f133d8e1e23d5dbc57509bf68dd8375418864dbb
Toowaa <brahanax@gmail.com>
2025-04-01 16:03:41 -0500

    Refactor SecurityConfig to remove unused imports and improve code cleanliness

--- src/main/java/com/bus/bus/security/SecurityConfig.java
+++ src/main/java/com/bus/bus/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.bus.bus.security;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,9 +16,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
